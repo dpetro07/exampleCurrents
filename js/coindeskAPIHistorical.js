8bd047d2458566d67152e94719fb5cb0bb54fb2d
ceckenrode <chriseckenrode@me.com>
2015-12-21 14:56:31 -0500

    changed date picker to pickadate, also created a function that checks that the user is choosig a suitable date range

--- js/coindeskAPIHistorical.js
+++ js/coindeskAPIHistorical.js
@@ -5,16 +5,18 @@ var endingDatePrefix = "&end=";
 var histCur;
 //setting up some global variables that will need to be used by several functions
 
-//dateChanger checks that both date fields are filled. if so, we call the getHistData function
+//dateChanger checks that both date fields are filled. if so, we call the getHistData function, also sets the global variables 
+//startingDate and endingDate
+
 function dateChanger() {
     if ($("#startingDate").val() !== "" && $("#endingDate").val() !== "") {
         startingDate = $("#startingDate").val();
         console.log(startingDate);
         endingDate = $("#endingDate").val();
         console.log(endingDate);
-        startingDate = startingDatePrefix + startingDate;
-        endingDate = endingDatePrefix + endingDate;
-        getHistData(histCur, startingDate, endingDate);
+        var ajaxStart = startingDatePrefix + startingDate;
+        var ajaxEnd = endingDatePrefix + endingDate;
+        getHistData(histCur, ajaxStart, ajaxEnd);
     } else {
         return;
     }
@@ -29,7 +31,7 @@ function getHistData(cur, start, end) {
     $.ajax({
         dataType: "json",
         type: "GET",
-        url: url + startingDate + endingDate,
+        url: url + start + end,
         data: {
             currency: histCur
         },
@@ -38,41 +40,105 @@ function getHistData(cur, start, end) {
         },
         error: function(jqXHR, textStatus, errorThrown) {
             console.log("Unable to retrieve data, some depencies could not be loaded.");
+            scopeChecker();
         }
     });
 
 }
 
+//function that makes sure user is choossing an end date that is greater than their start date
+function scopeChecker() {
+        var startVal = $("#startingDate").val();
+        var endVal = $("#endingDate").val();
+
+        var startArr = startVal.split('-');
+        var startYr = startArr[0];
+        var startMo = startArr[1];
+        var startDa = startArr[2];
+
+        var endArr = endVal.split('-');
+        var endYr = endArr[0];
+        var endMo = endArr[1];
+        var endDa = endArr[2];
+
+        if (startYr > endYr) {
+            alert("Choose an end date that is greater than your start date.");
+            $("#startingDate").val("");
+            $("#endingDate").val("");
+        } else if (startMo > endMo) {
+            alert("Choose an end date that is greater than your start date.");
+            $("#startingDate").val("");
+            $("#endingDate").val("");
+        } else if (startDa > endDa) {
+            alert("Choose an end date that is greater than your start date.");
+            $("#startingDate").val("");
+            $("#endingDate").val("");
+        }
+    }
+
 //sets value of histCur when page loads, also finds todays date.
 $(document).ready(function() {
     histCur = $("#historicalCurrency").text();
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1;
-
-    var yyyy = today.getFullYear();
-    if(dd<10){
-        dd='0'+dd
-    } 
-    if(mm<10){
-        mm='0'+mm
-    } 
-    var today = yyyy+'-'+mm+'-'+dd;
-    console.log(today);
+  
 //calls dateChanger when the datepicker is changed
-    $("#startingDate").Zebra_DatePicker({
-        direction: ['2010-07-17', today],
-        onSelect: function() {
-            dateChanger();
+     function getYesterdaysDate() {
+  var date = new Date();
+  date.setDate(date.getDate()-1);
+  return date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate();
+  }
+
+  function getTodaysDate() {
+  var date = new Date();
+  return date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate();
+  }
+
+//creating variables for todays date and yesterdays date to help scope the datepickers
+  var todaysDate = getTodaysDate();
+
+  var yesterdaysDate = getYesterdaysDate();
+
+//sets creates a datepicker from the #startingDate input field
+//some functions of datepicker didnt work as intended, onSet for example, so this function fixes those inconsistencies
+  $('#startingDate').click(function () {
+    var oldInput = $(this); //save this for using in the onSet event
+    var newInput = $(this).clone(true); //clone old input to prevent weird changes from datepicker.js
+    $(this).pickadate({
+    format: 'yyyy-mm-dd',
+    min: '2010-07-17',
+    max: yesterdaysDate,
+        onSet: function (event) { //occurs when something is selected
+            dateChanger()
+            if(event.highlight == undefined){  //is false when changing months         
+            $(oldInput).next().remove(); //hide picker
+            $(newInput).val($(oldInput).val()); //save value from weird generated input
+            $(oldInput).before(newInput); //place new normal input
+            $(oldInput).remove(); // remove weird input
+            }
         }
-    });
-    //calls dateChanger when the datepicker is changed
-   $("#endingDate").Zebra_DatePicker({
-        direction: ['2010-07-17', today],
-        onSelect: function() {
-            dateChanger();
+    })
+});
+
+//sets creates a datepicker from the #endingDate input field
+//some functions of datepicker didnt work as intended, onSet for example, so this function fixes those inconsistencies
+  $('#endingDate').click(function () {
+    var oldInput = $(this); //save this for using in the onSet event
+    var newInput = $(this).clone(true); //clone old input to prevent weird changes from datepicker.js
+    $(this).pickadate({
+    format: 'yyyy-mm-dd',
+    min: '2010-07-17',
+    max: yesterdaysDate,
+        onSet: function (event) { //occurs when something is selected
+            dateChanger()
+            if(event.highlight == undefined){  //is false when changing months         
+            $(oldInput).next().remove(); //hide picker
+            $(newInput).val($(oldInput).val()); //save value from weird generated input
+            $(oldInput).before(newInput); //place new normal input
+            $(oldInput).remove(); // remove weird input
+            }
         }
-    }); 
+    })
+});
+
 //calls dateChanger when the the histCur is changed
     $("#USDCur").click(function(event) {
         event.preventDefault();
