a99f6b8203fb88f4ecf191b5c4fd4db1216b9e3c
ceckenrode <chriseckenrode@me.com>
2015-12-30 13:32:03 -0500

    styled page and made it more responsive, added logo

--- js/coindeskAPIHistorical.js
+++ js/coindeskAPIHistorical.js
@@ -1,191 +1,191 @@
 function dateChanger() {
-    var startingDatePrefix = "start=";
-    var endingDatePrefix = "&end=";
-    if (formValidator() === true || initialLoad === true) {
-        var ajaxStart = startingDatePrefix + startingDate;
-        var ajaxEnd = endingDatePrefix + endingDate;
-        getHistData(histCur, ajaxStart, ajaxEnd);
-        initalLoad = false;
-    }
+  var startingDatePrefix = "start=";
+  var endingDatePrefix = "&end=";
+  if (formValidator() === true || initialLoad === true) {
+    var ajaxStart = startingDatePrefix + startingDate;
+    var ajaxEnd = endingDatePrefix + endingDate;
+    getHistData(histCur, ajaxStart, ajaxEnd);
+    initalLoad = false;
+  }
 }
 
-    yesterdaysDate = getYesterdaysDate()
-    endingDate = getTodaysDate();
-    startingDate = getLastWeeksDate();
+yesterdaysDate = getYesterdaysDate()
+endingDate = getTodaysDate();
+startingDate = getLastWeeksDate();
 
-    dateChanger();
+dateChanger();
 
 //checks to see that both forms are filled before taking any action. 
 function formValidator() {
-    if ($("#startingDate").val() !== "" && $("#endingDate").val() !== "") {
-        startingDate = $("#startingDate").val();
-        console.log(startingDate);
-        endingDate = $("#endingDate").val();
-        console.log(endingDate);
-        return true;
-    } else {
-        return false
-    }
+  if ($("#startingDate").val() !== "" && $("#endingDate").val() !== "") {
+    startingDate = $("#startingDate").val();
+    console.log(startingDate);
+    endingDate = $("#endingDate").val();
+    console.log(endingDate);
+    return true;
+  } else {
+    return false
+  }
 
 }
 
-    function getYesterdaysDate() {
-        var date = new Date();
-        date.setDate(date.getDate() - 1);
-        return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-    }
+function getYesterdaysDate() {
+  var date = new Date();
+  date.setDate(date.getDate() - 1);
+  return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+}
 
-    function getLastWeeksDate() {
-        var date = new Date();
+function getLastWeeksDate() {
+  var date = new Date();
 
-        date.setDate(date.getDate() - 7);
-        return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-    }
+  date.setDate(date.getDate() - 7);
+  return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+}
 
-    function getTodaysDate() {
-        var date = new Date();
-        return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-    }
+function getTodaysDate() {
+  var date = new Date();
+  return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+}
 
 //we grab the currency selected, the start, and end dates and send them to the ajax request to return the historical data
 function getHistData(cur, start, end) {
 
-    var url = "https://api.coindesk.com/v1/bpi/historical/close.json?"
+  var url = "https://api.coindesk.com/v1/bpi/historical/close.json?"
 
-    $.ajax({
-        dataType: "json",
-        type: "GET",
-        url: url + start + end,
-        data: {
-            currency: histCur
-        },
-        success: function(data) {
-            chartRows = [];
-            histObject = data.bpi;
-            histDates = Object.keys(histObject);
-            histValues = [];
-            for (var i = 0; i < histDates.length; i++) {
-            var val = histObject[histDates[i]];
-            histValues.push(val);
-                }
-            for (var i = 0; i < histValues.length; i++) {
-            chartRows.push([histDates[i], histValues[i]]);
-            };
-            if (initalLoad === false) {
-                drawChart();
-            }
-            console.log(histDates);
-            console.log(histValues);
-            console.log(histObject);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log("Unable to retrieve data, some depencies could not be loaded.");
-        }
-    });
+  $.ajax({
+    dataType: "json",
+    type: "GET",
+    url: url + start + end,
+    data: {
+      currency: histCur
+    },
+    success: function(data) {
+      chartRows = [];
+      histObject = data.bpi;
+      histDates = Object.keys(histObject);
+      histValues = [];
+      for (var i = 0; i < histDates.length; i++) {
+        var val = histObject[histDates[i]];
+        histValues.push(val);
+      }
+      for (var i = 0; i < histValues.length; i++) {
+        chartRows.push([histDates[i], histValues[i]]);
+      };
+      if (initalLoad === false) {
+        drawChart();
+      }
+      console.log(histDates);
+      console.log(histValues);
+      console.log(histObject);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log("Unable to retrieve data, some depencies could not be loaded.");
+    }
+  });
 
 }
 
 //function that makes sure user is choossing an end date that is greater than their start date
 function scopeChecker() {
-    var startVal = $("#startingDate").val();
-    var endVal = $("#endingDate").val();
+  var startVal = $("#startingDate").val();
+  var endVal = $("#endingDate").val();
 
-    var startArr = startVal.split('-');
-    var startYr = startArr[0];
-    var startMo = startArr[1];
-    var startDa = startArr[2];
+  var startArr = startVal.split('-');
+  var startYr = startArr[0];
+  var startMo = startArr[1];
+  var startDa = startArr[2];
 
-    var endArr = endVal.split('-');
-    var endYr = endArr[0];
-    var endMo = endArr[1];
-    var endDa = endArr[2];
+  var endArr = endVal.split('-');
+  var endYr = endArr[0];
+  var endMo = endArr[1];
+  var endDa = endArr[2];
 
-    if (startYr > endYr) {
-        bootbox.alert("Choose an end date that is greater than your start date.");
-        $("#startingDate").val("");
-        $("#endingDate").val("");
-    } else if (startMo > endMo) {
-        bootbox.alert("Choose an end date that is greater than your start date.");
-        $("#startingDate").val("");
-        $("#endingDate").val("");
-    } else if (startDa > endDa) {
-        bootbox.alert("Choose an end date that is greater than your start date.");
-        $("#startingDate").val("");
-        $("#endingDate").val("");
-    } else {
-        dateChanger();
-    }
+  if (startYr > endYr) {
+    bootbox.alert("Choose an end date that is greater than your start date.");
+    $("#startingDate").val("");
+    $("#endingDate").val("");
+  } else if (startMo > endMo) {
+    bootbox.alert("Choose an end date that is greater than your start date.");
+    $("#startingDate").val("");
+    $("#endingDate").val("");
+  } else if (startDa > endDa) {
+    bootbox.alert("Choose an end date that is greater than your start date.");
+    $("#startingDate").val("");
+    $("#endingDate").val("");
+  } else {
+    dateChanger();
+  }
 }
 
 //sets value of histCur when page loads, also finds todays date.
 $(document).ready(function() {
-    histCur = $("#historicalCurrency").text();
-    
-    //setting values for yesterdays date, and the initial ending and starting date to be one week apart to show
-    //the initial graph data
+  histCur = $("#historicalCurrency").text();
 
-    //sets creates a datepicker from the #startingDate input field
-    //some functions of datepicker didnt work as intended, onSet for example, so this function fixes those inconsistencies
-    $('#startingDate').click(function() {
-        var oldInput = $(this); //save this for using in the onSet event
-        var newInput = $(this).clone(true); //clone old input to prevent weird changes from datepicker.js
-        $(this).pickadate({
-            format: 'yyyy-mm-dd',
-            min: '2010-07-17',
-            max: yesterdaysDate,
-            onSet: function(event) { //occurs when something is selected
-                dateChanger();
-                if (event.highlight == undefined) { //is false when changing months         
-                    $(oldInput).next().remove(); //hide picker
-                    $(newInput).val($(oldInput).val()); //save value from weird generated input
-                    $(oldInput).before(newInput); //place new normal input
-                    $(oldInput).remove(); // remove weird input
-                }
-            }
-        })
-    });
+  //setting values for yesterdays date, and the initial ending and starting date to be one week apart to show
+  //the initial graph data
 
-    //sets creates a datepicker from the #endingDate input field
-    //some functions of datepicker didnt work as intended, onSet for example, so this function fixes those inconsistencies
-    $('#endingDate').click(function() {
-        var oldInput = $(this); //save this for using in the onSet event
-        var newInput = $(this).clone(true); //clone old input to prevent weird changes from datepicker.js
-        $(this).pickadate({
-            format: 'yyyy-mm-dd',
-            min: '2010-07-17',
-            max: yesterdaysDate,
-            onSet: function(event) { //occurs when something is selected
-                scopeChecker()
-                if (event.highlight == undefined) { //is false when changing months         
-                    $(oldInput).next().remove(); //hide picker
-                    $(newInput).val($(oldInput).val()); //save value from weird generated input
-                    $(oldInput).before(newInput); //place new normal input
-                    $(oldInput).remove(); // remove weird input
-                }
-            }
-        })
-    });
+  //sets creates a datepicker from the #startingDate input field
+  //some functions of datepicker didnt work as intended, onSet for example, so this function fixes those inconsistencies
+  $('#startingDate').click(function() {
+    var oldInput = $(this); //save this for using in the onSet event
+    var newInput = $(this).clone(true); //clone old input to prevent weird changes from datepicker.js
+    $(this).pickadate({
+      format: 'yyyy-mm-dd',
+      min: '2010-07-17',
+      max: yesterdaysDate,
+      onSet: function(event) { //occurs when something is selected
+        dateChanger();
+        if (event.highlight == undefined) { //is false when changing months         
+          $(oldInput).next().remove(); //hide picker
+          $(newInput).val($(oldInput).val()); //save value from weird generated input
+          $(oldInput).before(newInput); //place new normal input
+          $(oldInput).remove(); // remove weird input
+        }
+      }
+    })
+  });
 
-    //calls dateChanger when the the histCur is changed
-    $("#USDCur").click(function(event) {
-        event.preventDefault();
-        $("#historicalCurrency").text("USD");
-        histCur = "USD";
-        dateChanger();
-    });
-    //calls dateChanger when the the histCur is changed
-    $("#GBPCur").click(function(event) {
-        event.preventDefault();
-        $("#historicalCurrency").text("GBP");
-        histCur = "GBP";
-        dateChanger();
-    });
-    //calls dateChanger when the the histCur is changed
-    $("#EURCur").click(function(event) {
-        event.preventDefault();
-        $("#historicalCurrency").text("EUR");
-        histCur = "EUR";
-        dateChanger();
-    });
+  //sets creates a datepicker from the #endingDate input field
+  //some functions of datepicker didnt work as intended, onSet for example, so this function fixes those inconsistencies
+  $('#endingDate').click(function() {
+    var oldInput = $(this); //save this for using in the onSet event
+    var newInput = $(this).clone(true); //clone old input to prevent weird changes from datepicker.js
+    $(this).pickadate({
+      format: 'yyyy-mm-dd',
+      min: '2010-07-17',
+      max: yesterdaysDate,
+      onSet: function(event) { //occurs when something is selected
+        scopeChecker()
+        if (event.highlight == undefined) { //is false when changing months         
+          $(oldInput).next().remove(); //hide picker
+          $(newInput).val($(oldInput).val()); //save value from weird generated input
+          $(oldInput).before(newInput); //place new normal input
+          $(oldInput).remove(); // remove weird input
+        }
+      }
+    })
+  });
+
+  //calls dateChanger when the the histCur is changed
+  $("#USDCur").click(function(event) {
+    event.preventDefault();
+    $("#historicalCurrency").text("USD");
+    histCur = "USD";
+    dateChanger();
+  });
+  //calls dateChanger when the the histCur is changed
+  $("#GBPCur").click(function(event) {
+    event.preventDefault();
+    $("#historicalCurrency").text("GBP");
+    histCur = "GBP";
+    dateChanger();
+  });
+  //calls dateChanger when the the histCur is changed
+  $("#EURCur").click(function(event) {
+    event.preventDefault();
+    $("#historicalCurrency").text("EUR");
+    histCur = "EUR";
+    dateChanger();
+  });
 
 });
--- js/googleCharts.js
+++ js/googleCharts.js
@@ -1,23 +1,25 @@
-      function drawChart() {
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Date');
-      data.addColumn('number', 'Price');
-      data.addRows(chartRows);
+function drawChart() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Date');
+  data.addColumn('number', 'Price');
+  data.addRows(chartRows);
 
-      // Instantiate and draw the chart.
-      var chart = new google.visualization.LineChart(document.getElementById('myLineChart'));
-      chart.draw(data, null);
-      
-    }
-    
- 
-  
-      
-  $(document).ready(function() {
+  // Instantiate and draw the chart.
+  var chart = new google.visualization.LineChart(document.getElementById('myLineChart'));
+  chart.draw(data, null);
 
-      google.charts.load('current', {packages: ['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+}
 
-    
-    
-      });
+
+
+
+$(document).ready(function() {
+
+  google.charts.load('current', {
+    packages: ['corechart']
+  });
+  google.charts.setOnLoadCallback(drawChart);
+
+
+
+});
